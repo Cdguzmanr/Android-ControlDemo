673ffec9881a95985e89781c0f084b4333d1294e
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-01-23 12:19:34 -0600

    Making sure

--- app/src/main/java/edu/fvtc/controldemo/MainActivity.java
+++ app/src/main/java/edu/fvtc/controldemo/MainActivity.java
@@ -1,13 +1,20 @@
+
 package edu.fvtc.controldemo;
 
+// using = import
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+// In java, "extends" is used for inheritance
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // super class = base class
         super.onCreate(savedInstanceState);
+        // Make an activity from the xml
         setContentView(R.layout.activity_main);
+                    // R = res folder
     }
 }
